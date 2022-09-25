178ddcce666ae2deca2c44343a86bcafe0da940c
Federico Douglas <fededouglas@hotmail.com>
2022-09-25 19:23:59 -0300

    Master

---
+++ GranLibro/Geolocation-API/Geolocation_Ej2/geolocation.js
@@ -0,0 +1,24 @@
+function iniciar() {
+    var boton = document.getElementById('obtener');
+    boton.addEventListener('click', obtener, false);
+}
+function obtener() {
+    var geoconfig = {
+        enableHighAccuracy: true,
+        maximumAge: 60000
+    };
+    control = navigator.geolocation.watchPosition(mostrar, errores,
+        geoconfig);
+}
+function mostrar(posicion) {
+    var ubicacion = document.getElementById('ubicacion');
+    var datos = '';
+    datos += 'Latitud: ' + posicion.coords.latitude + '<br>';
+    datos += 'Longitud: ' + posicion.coords.longitude + '<br>';
+    datos += 'Exactitud: ' + posicion.coords.accuracy + 'mts.<br>';
+    ubicacion.innerHTML = datos;
+}
+function errores(error) {
+    alert('Error: ' + error.code + ' ' + error.message);
+}
+window.addEventListener('load', iniciar, false);
---
+++ GranLibro/Geolocation-API/Geolocation_Ej3/geolocation.js
@@ -0,0 +1,28 @@
+function iniciar() {
+    var boton = document.getElementById('obtener');
+    boton.addEventListener('click', obtener, false);
+}
+
+function obtener() {
+    var geoconfig = {
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 60000
+    };
+    navigator.geolocation.getCurrentPosition(mostrar, errores,
+        geoconfig);
+}
+
+function mostrar(posicion) {
+    var ubicacion = document.getElementById('ubicacion');
+    var datos = '';
+    datos += 'Latitud: ' + posicion.coords.latitude + '<br>';
+    datos += 'Longitud: ' + posicion.coords.longitude + '<br>';
+    datos += 'Exactitud: ' + posicion.coords.accuracy + 'mts.<br>';
+    ubicacion.innerHTML = datos;
+}
+
+function errores(error) {
+    alert('Error: ' + error.code + ' ' + error.message);
+}
+window.addEventListener('load', iniciar, false);
