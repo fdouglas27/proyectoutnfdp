b936dc0aca040819813d2e0068790b853c334dcb
fdouglas27 <fededouglas@hotmail.com>
2023-01-04 16:42:26 -0300

    Cambios varios del 04-01-2023 sobre db.js usersModel.js otros en views y otros en routes

--- babytravel_backend/routes/admin/login.js
+++ babytravel_backend/routes/admin/login.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var usersModel = require('../../models/usersModel'); //no necesito traer la base de datos porque la llamo en usersModel.js
 
 router.get('/', function (req, res, next) {
     res.render('admin/login', {
         layout: 'admin/layout'
     });
 });
+
+router.post('/', async (req, res, next) => {
+    try {
+        var user = req.body.user;
+        var password = req.body.password;
+
+        var data = await usersModel.getUserByUsernameAndPassword(user, password);
+
+        if (data != undefined) {
+            res.redirect('/admin/novedades');
+        } else {
+            res.render('admin/login', {
+                layout: 'admin/layout',
+                error: true
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
 
 module.exports = router;
